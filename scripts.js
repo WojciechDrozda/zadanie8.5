d553f5e70ea6afccc4825ece41de3188affd95dd
Wojciech Drozda <wojciech.drozdaa@gmail.com>
2017-06-06 23:38:46 +0200

    Zadanie5

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('Wpisz długosc boku');
+var h = prompt('Wpisz dlugosc wysokosci');
+var triangleArea;
+
+triangleArea = a*h/2;
+alert('Pole trojkata to : ' + triangleArea +'.');
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
